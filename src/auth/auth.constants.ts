d9886bc3495bccc39f5a35534c4012c6f5930da3
bakai <joldoshovbakai04@gmail.com>
2024-04-17 12:01:19 +0600

    feat: auth login and registration

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALLREADY_REGISTERED_MESSAGE = 'Этот email уже существует';
+export const USER_NOT_FOUND = 'Пользователь с таким email не существует';
+export const WRONG_PASSWORD = 'Пароль не совпадает';
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,12 +7,15 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND_MESSAGE } from './review.constants';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -34,8 +37,13 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
+  async getByProduct(
+    @Param('productId') productId: string,
+    @UserEmail() email: string,
+  ) {
+    console.log(email);
     return this.reviewService.findByProductId(productId);
   }
 }
